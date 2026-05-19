d87fc36048235a387f030afa8e4294b8a9cc82b8
bassantAli <basant.ali123zx@gmail.com>
2026-05-19 18:29:06 +0300

    create server failure and add dartz

--- lib/features/category/data/repos/category_repo_impl.dart
+++ lib/features/category/data/repos/category_repo_impl.dart
@@ -1,18 +1,27 @@
 import 'package:books_app/core/utils/app_service.dart';
+import 'package:books_app/core/utils/errors.dart';
 import 'package:books_app/core/utils/service_locator.dart';
 import 'package:books_app/features/category/data/models/book_category_model.dart';
 import 'package:books_app/features/category/data/repos/category_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class CategoryRepoImpl implements CategoryRepo {
   final repo = getIt<AppService>();
   final String url = 'https://openlibrary.org/subjects/';
   @override
-  Future<List<BookModel>> fetchCategory(String categoryName) async {
-    var data = await repo.get('$url+$categoryName.json');
-    List<BookModel> books = [];
-    for (var item in data['works']) {
-      books.add(BookModel.fromJson(item));
+  Future<Either<Failure, List<BookModel>>> fetchCategory(
+    String categoryName,
+  ) async {
+    try {
+      var data = await repo.get('$url+$categoryName.json');
+      List<BookModel> books = [];
+      for (var item in data['works']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDioException(e));
     }
-    return books;
   }
 }
